6510675a67ebe7bcb767a54c8f7ed2e9e34e1369
softdev-ebuka <agbimgreat@gmail.com>
2023-03-23 15:19:40 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ *
+ * @n: the number of lines to draw
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0 ; x < n ; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
