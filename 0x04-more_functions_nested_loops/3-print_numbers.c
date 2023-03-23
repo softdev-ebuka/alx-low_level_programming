c0e1a02bb75c70011f47079aec400aef80eb28f3
softdev-ebuka <agbimgreat@gmail.com>
2023-03-23 13:01:02 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,15 @@
 /**
  * print_numbers - print the numbers from 0 to 9
  *
- * Return: void
+ * Return: 0
  */
 void print_numbers(void)
 {
 	int x;
 
 	for (x = 0 ; x <= 9 ; x++)
-		_putchar(x + '0');
-	_putchar('\n')
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
 }
