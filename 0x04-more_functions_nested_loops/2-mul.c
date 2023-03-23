d419d1ae42979b716aad49692bf478eb8189e63b
softdev-ebuka <agbimgreat@gmail.com>
2023-03-23 11:56:11 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  mul - multipky two integers
+ *
+ *  @a: first number used for the multiplication
+ *  @b: secon number to multiply with the first
+ *
+ *  Return: the result of multiplication of both parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
