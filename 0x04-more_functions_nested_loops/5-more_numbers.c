4897916ada0c970363a42ba43713b4cfdc8a7f58
softdev-ebuka <agbimgreat@gmail.com>
2023-03-23 14:24:16 -0700

    10x the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10x the numbers fron 0 to 14
+ *
+ * Return: 10x of the numbers 0 t0 14
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = 0 ; y <= 14 ; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
+		}
+	}
+	_putchar('\n');
+}
