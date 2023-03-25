ae863d5e19a02dc183427162b51bbe00979d51ad
softdev-ebuka <agbimgreat@gmail.com>
2023-03-25 13:45:39 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two values
+ *
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+ *
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
