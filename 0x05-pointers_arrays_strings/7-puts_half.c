f35e4df70caf9abfaaf357245213472fe625ecf4
softdev-ebuka <agbimgreat@gmail.com>
2023-03-27 14:56:42 -0700

    half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: function parameter
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int x = 0;
+	int y;
+
+	while (str[x] != '\0')
+		x++;
+	if (x % 2 == 1)
+	{
+		y = (x - 1) / 2;
+		y = += 1;
+	}
+	else
+		y = x / 2;
+	for (; y < x ; y++)
+		_putchar(str[y]);
+	_putchar('\n');
+}
