d024fd4ec407fc94914ef5fbde9622e7c821abaf
softdev-ebuka <agbimgreat@gmail.com>
2023-03-27 14:19:11 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,11 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	int count = 0;
+	int i = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-		count++;
-	for (i = count ; i >= 0 ; i--)
+	while (s[i] != '\0')
+		i++;
+	for (i -= 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
