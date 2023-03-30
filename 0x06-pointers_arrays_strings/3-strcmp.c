83e2b07a1788ec17a69af0753e05f12c5851efdf
softdev-ebuka <agbimgreat@gmail.com>
2023-03-30 15:24:20 -0700

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x;
-
-	for (x = 0 ; s1[x] != '\0' || s2[x] != '\0' ; x++)
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		if (s1[x] != s2[x])
-		{
-			if (s1[x] < s2[x])
-				return (s1[x] - s2[x]);
-			else if (s1[x] > s2[x])
-				return (s1[x] - s2[x]);
-		}
-		else
-			return (0);
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
