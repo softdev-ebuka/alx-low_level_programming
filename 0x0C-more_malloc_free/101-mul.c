7fb4a9b0dafccfd186dc668e4f905c70857ad36d
softdev-ebuka <agbimgreat@gmail.com>
2023-04-14 15:22:25 -0700

    multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,9 +21,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	for (i = 1; i < argc; i++)
+	for (i = 1 ; i < argc ; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
