1ff39cb84e91d9612f24918c8fb0c5f92a350d66
softdev-ebuka <agbimgreat@gmail.com>
2023-04-20 16:39:24 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 	unsigned int x, sum = 0;
 
-	if (n == 0)
-		return (0);
 	va_start(list, n);
 	if (n != 0)
 		for (x = 0 ; x < n ; x++)
