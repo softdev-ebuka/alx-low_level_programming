57567b657503e616fd1982b2587ad6a92e1d6612
softdev-ebuka <agbimgreat@gmail.com>
2023-04-28 19:32:50 -0700

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,24 +11,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (!head || !str)
-		return (NULL);
+	list_t *new_node;
+	
+	if (head == NULL || str == NULL)
+	return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
-	if (!new_node)
-		return (NULL);
+	if (new_node == NULL)
+	return (NULL);
 
-	char *str_copy = strdup(str);
-
-	if (!str_copy)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(new_node);
-		return (NULL);
+	free(new_node);
+	return (NULL);
 	}
-	new_node->str = str_copy;
-	new_node->len = strlen(str)
+
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
 }
