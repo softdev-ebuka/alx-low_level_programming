de5686ee6362bd5d833bdc38c746da3ffec4274b
softdev-ebuka <agbimgreat@gmail.com>
2023-04-28 21:40:40 -0700

    free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list-t list
+ * @head: head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
+}
