ed9a3f024ce84835bc3c1c9f43e39889384d82b0
softdev-ebuka <agbimgreat@gmail.com>
2023-04-28 22:46:27 -0700

    add last node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->str = strdup(str);
 
 	if (!new_node->str)
@@ -32,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
-	if (!*head) 
+	if (!*head)
 	{
 		*head = new_node;
 	}
