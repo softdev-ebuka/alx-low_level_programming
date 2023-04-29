576ce5c6285fc9872b8905fb79b12f9e2d54a107
softdev-ebuka <agbimgreat@gmail.com>
2023-04-28 22:28:00 -0700

    assemmbly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+hello db "Hello, Holberton", 0
+
+section .text
+global main
+extern printf
+
+main:
+push rbp ; set up stack frame
+mov rbp, rsp
+mov rdi, hello ; 1st argument: address of the format string
+xor eax, eax ; clear EAX register to indicate no floating point arguments
+call printf ; call printf function
+xor eax, eax ; set return value to 0
+pop rbp ; tear down stack frame
+ret 
