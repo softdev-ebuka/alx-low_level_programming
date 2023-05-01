67bffb8f81425f6878b8ab0387154d6376320ece
softdev-ebuka <agbimgreat@gmail.com>
2023-05-01 12:56:09 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (x = 0 ; x < n ; x++)
 	{
 		printf("%d", va_arg(list, int));
-		if (x < n - 1 && seperator != NULL)
-			printf("%s", seperator);
+		if (x != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(list);
