55e59af4179361a8d562538ab1a0f8b0ccf23564
softdev-ebuka <agbimgreat@gmail.com>
2023-05-02 10:50:03 -0700

    frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,11 +14,11 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		node = head->next;
-		free(head);
-		head = node;
+		node = *head;
+		*head = (*head)->next;
+		free(node);
 	}
 
 	*head = NULL;
