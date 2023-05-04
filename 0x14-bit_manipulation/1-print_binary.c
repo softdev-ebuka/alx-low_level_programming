f4d81636fcaaba06e418dce00b4d658e8b7da3a2
softdev-ebuka <agbimgreat@gmail.com>
2023-05-04 10:43:59 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,19 +12,22 @@ void print_binary(unsigned long int n)
 	unsigned long int max = 1;
 	int len = sizeof(unsigned long int) * 8;
 	int x = len - 1;
+	int flag = 0;
 
 	while (x >= 0)
 	{
-		if ((n & (max << x)) == 0 && x != 0)
-		{
-			x--;
-			continue;
-		}
-
-		if ((n & (max << x)) != 0)
-			_putchar('1');
+		if ((n & (max << x)) == 0 && !flag && x != 0)
+			;
 		else
-			_putchar('0');
+		{
+			if ((n & (max << x)) == 0)
+				_putchar('0');
+			else
+			{
+				_putchar('1');
+				flag = 1;
+			}
+		}
 		x--;
 	}
 }
